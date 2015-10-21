682d8f092e930c5b8e0447299ef50fb7ecd967c9
lucavictor220 <lucavictor220@gmail.com>
2015-10-21 13:24:54 +0300

    Added pagination

--- public/app/index.module.js
+++ public/app/index.module.js
@@ -2,6 +2,6 @@
   'use strict';
 
   angular
-    .module('courtApp', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'ui.bootstrap', 'toastr']);
+    .module('courtApp', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'ui.bootstrap', 'toastr', 'angularUtils.directives.dirPagination']);
 
 })();
